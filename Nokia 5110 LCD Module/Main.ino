6a1818a4ab7ab64eb842b75917a3fc10569e80a1
Saumitra Kapoor <saumitrakapoor21@gmail.com>
2017-01-26 15:54:29 +0530

    Update Main.ino

--- Nokia 5110 LCD Module/Main.ino
+++ Nokia 5110 LCD Module/Main.ino
@@ -1,1 +1,18 @@
+#include <PCD8544.h>
+static const byte glyph[] = { B00010000, B00110100, B00110000, B00110100, B00010000 };
 
+static PCD8544 lcd;
+
+void setup() {
+
+  lcd.begin(84, 48);
+  lcd.createChar(0, glyph);
+}
+
+void loop() {
+
+  lcd.setCursor(0, 0);
+  lcd.print("Hello World");
+   lcd.setCursor(0, 1);
+   lcd.print(":)");
+}
